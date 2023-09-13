de3907c47bf7bb5c2e8c4348fbe8f9d3fb0a71c1
lionel-alvarado16 <lionel.alex64@hotmail.com>
2023-09-12 23:13:44 -0500

    Finished prep 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -11,10 +11,10 @@ class Program
         string name = Console.ReadLine();
 
         Console.Write("What is your last name? ");
-        string last_name = Console.ReadLine();
+        string lastName = Console.ReadLine();
         Console.WriteLine();
 
-        Console.WriteLine($"Your name is {last_name}, {name} {last_name}.");
+        Console.WriteLine($"Your name is {lastName}, {name} {lastName}.");
         // Console.WriteLine();
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,64 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("Please enter your grade percentage: ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+        string letter = "";
+
+        // Getting the letter grade
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >=80)
+        {
+            letter = "B";
+        }
+        else if (grade >=70)
+        {
+            letter = "C";
+        }
+        else if (grade >=60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Getting the sign
+
+        int lastDigit = grade % 10;
+        string sign = "";
+
+        if (lastDigit >= 7 && letter != "A" && letter != "F")
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3 && letter != "F")
+        {
+            sign = "-";
+        }
+        else
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{sign}.");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("We're sorry, you can do better next time. Keep going!");
+        }
     }
 }
