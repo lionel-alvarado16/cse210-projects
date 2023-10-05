b1e7a9da2255bdc68e1a88653bcf951cfce0f5b8
lionel-alvarado16 <lionel.alex64@hotmail.com>
2023-10-05 03:48:06 -0500

    Progress Project

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        // Console.WriteLine("Hello Develop02 World!");
+
+        List<string> lista = new List<string> {"manzana", "naranja", "pera", "uva"};
+        Random randomGenerator = new Random();
+        int index = randomGenerator.Next(lista.Count);
+        string prompt = lista[index];
+
+        Console.WriteLine(prompt);
     }
 }
