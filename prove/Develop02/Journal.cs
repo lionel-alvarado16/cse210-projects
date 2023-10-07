4800f49193065ca72eedcb208dc61dc528c7879b
lionel-alvarado16 <lionel.alex64@hotmail.com>
2023-10-07 01:40:15 -0500

    Almost finished

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 public class Journal
 {
@@ -34,13 +36,36 @@ public class Journal
         }
     }
 
-    public void LoadFile()
+    public List<Entry> LoadFile()
     {
+        Console.Write("What is the filename? ");
+        string fileName = Console.ReadLine();
+        string[] lines = System.IO.File.ReadAllLines(fileName);
 
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("~");
+            
+            Entry newEntry = new Entry();     
+            newEntry._prompt = parts[0];
+            newEntry._entry = parts[1];
+            _entries.Add(newEntry);
+        }
+
+        return _entries;
     }
 
     public void SaveFile()
     {
+        Console.Write("What is the filename? ");
+        string fileName = Console.ReadLine();
 
+        using (StreamWriter outputFile = new StreamWriter(fileName))
+        {
+            foreach (Entry ent in _entries)
+            {
+                outputFile.WriteLine($"{ent._prompt}~{ent._entry}");
+            }
+        }
     }
 }
