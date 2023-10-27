5960a02eaab1f35d0c503b6aba0d9d2b4b546396
lionel-alvarado16 <lionel.alex64@hotmail.com>
2023-10-27 01:53:27 -0500

    Learnig 04 Complete

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -6,12 +6,12 @@ class Program
     {
         // Console.WriteLine("Hello Learning04 World!");
 
-        // Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
-        // Console.WriteLine(assignment.GetSummary());
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
 
-        // MathAssignment mathAssignment = new MathAssignment("Samuel Bennett", "Multiplication", "7.3", "8-19");
-        // Console.WriteLine(mathAssignment.GetSummary());
-        // Console.WriteLine(mathAssignment.GetHomeworkList());
+        MathAssignment mathAssignment = new MathAssignment("Samuel Bennett", "Multiplication", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
 
         WritingAssignment writingAssignment = new WritingAssignment("Mary Water", "European History", "The Causes of World War II");
         Console.WriteLine(writingAssignment.GetSummary());
